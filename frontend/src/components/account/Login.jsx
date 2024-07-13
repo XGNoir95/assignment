c47c62aef4c9917c162419fb98a53ccdf0af9adf
Sunny <sanjidasunny25@gmail.com>
2024-07-12 23:07:44 -0600

    5

--- frontend/src/components/account/Login.jsx
+++ frontend/src/components/account/Login.jsx
@@ -97,11 +97,11 @@ const Login = ({ isUserAuthenticated }) => {
             let response = await API.userLogin(login);
             if (response.isSuccess) {
                 showError('');
-
+    
                 sessionStorage.setItem('accessToken', `Bearer ${response.data.accessToken}`);
                 sessionStorage.setItem('refreshToken', `Bearer ${response.data.refreshToken}`);
                 setAccount({ name: response.data.name, username: response.data.username });
-
+    
                 isUserAuthenticated(true);
                 setLogin(loginInitialValues);
                 navigate('/');
@@ -113,6 +113,7 @@ const Login = ({ isUserAuthenticated }) => {
             console.log(error);
         }
     };
+    
 
     const signupUser = async () => {
         try {
